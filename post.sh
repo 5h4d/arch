2438ec1790c7514237fa2ec2aa054f82a7bfd9b1
5h4d <62404657+5h4d@users.noreply.github.com>
2020-11-12 14:21:55 +0100

    Create post.sh

---
+++ post.sh
@@ -0,0 +1,23 @@
+ln -sf /usr/share/zoneinfo/Europe/Bratislava /etc/localtime
+hwclock --systohc
+sed -i '/en_US.UTF-8 UTF-8/s/^#//g'
+sed -i '/sk_SK.UTF-8 UTF-8/s/^#//g'
+locale-gen
+echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+echo 'arch' >> /etc/hostname
+echo '127.0.0.1     localhost' >> /etc/hosts
+echo '::1           localhost' >> /etc/hosts
+echo '127.0.1.1     arch.localdomain    arch' >> /etc/hosts
+echo 'Root password'
+passwd
+useradd yee
+usermod -aG wheel,audio,video,optical,storage yee
+echo 'yee password'
+passwd yee
+sed -i '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers.tmp
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager
+systemctl enable gdm.service
+pacman -S wine lutris steam discord lib32-gnutls qbittorrent
+echo 'All done, exit chroot and off you go'
