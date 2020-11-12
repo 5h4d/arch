e4bcacfd1a9d050502c6c8cb060c51e7af919df2
5h4d <62404657+5h4d@users.noreply.github.com>
2020-11-12 15:21:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+read -p 'Are you sure you want to install Arch to /dev/sda? [y/N]:' checkcorrectdisk
+if ! [checkcorrectdisk = 'y'] && ! [checkcorrectdisk = 'Y']
+then 
+  echo 'Edit script to install to a different drive or partitions'
+  exit
+fi
+
 timedatectl set-ntp true
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
   g
