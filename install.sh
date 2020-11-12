0601a7a433aa9e35ea3630568c976cad3c47c6ea
5h4d <62404657+5h4d@users.noreply.github.com>
2020-11-12 11:39:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
-fdisk ${TGTDEV}
-g
-n
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+  g
+  n
 
 
-+550M
-n
+  +550M
+  n
 
-+4G
-n
+  +4G
+  n
 
 
 
-t
-1
-1
-t
-2
-19
-w
+  t
+  1
+  1
+  t
+  2
+  19
+  w
+EOF
