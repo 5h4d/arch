8751936e99b818f01ba05e28b1359c2dc6ee3eb5
5h4d <62404657+5h4d@users.noreply.github.com>
2020-11-12 18:33:02 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,25 @@
+ln -sf /usr/share/zoneinfo/Europe/Bratislava /etc/localtime
+hwclock --systohc
+sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
+sed -i '/sk_SK.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+echo 'arch' >> /etc/hostname
+echo '127.0.0.1     localhost' >> /etc/hosts
+echo '::1           localhost' >> /etc/hosts
+echo '127.0.1.1     arch.localdomain    arch' >> /etc/hosts
+echo 'Root password'
+passwd
+read -p 'What name do you want the regular user to have?' username
+useradd $username
+pacman -S sudo
+y
+usermod -aG wheel,audio,video,optical,storage $username
+echo 'Password for regular user'
+passwd $username
+sed -i '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager
+systemctl enable gdm.service
+echo 'All done, exit chroot and off you go'
