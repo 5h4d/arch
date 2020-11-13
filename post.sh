5e01a203285d9fa45ff83567161f98cc5d42ad52
5h4d <62404657+5h4d@users.noreply.github.com>
2020-11-13 10:19:42 +0100

    Update post.sh

--- post.sh
+++ post.sh
@@ -16,6 +16,8 @@ usermod -aG wheel,audio,video,optical,storage $username
 echo 'Password for '$username
 passwd $username
 sed -i '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers
+mkdir /home/$username
+chown $username:$username /home/$username
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
