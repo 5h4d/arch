3fda49392f08ba5af0e25ff0ccf09e8bc12eec50
5h4d <62404657+5h4d@users.noreply.github.com>
2020-11-13 11:42:46 +0100

    Update gui-programs.sh

--- gui-programs.sh
+++ gui-programs.sh
@@ -2,6 +2,9 @@
 echo 'This script will install: wine, lutris, steam, discord, gparted, qbittorrent, spotify, google chrome, python, idle, and some gnome extensions'
 echo 'Tap enter to continue...'
 read tmpvar
+sudo sed -i '/[multilib]/s/^#//g' /etc/pacman.conf
+sudo sed -i '/Include = /etc/pacman.d/mirrorlist/s/^#//g' /etc/pacman.conf
+sudo pacman -Syu
 sudo pacman -S wine lutris steam discord gparted qbittorrent gnome-shell-extensions python
 git clone http://aur.archlinux.org/yay-git.git
 git clone https://aur.archlinux.org/gnome-shell-extension-ubuntu-dock.git
@@ -23,7 +26,4 @@ sudo rm -r spotify yay-git gnome-shell-extension-ubuntu-dock idle-python3.7-asse
 unzip -d ~/.local/share/gnome-shell/extensions extensions.zip
 rm extensions.zip
 yay google-chrome
-sudo sed -i '/[multilib]/s/^#//g' /etc/pacman.conf
-sudo sed -i '/Include = /etc/pacman.d/mirrorlist/s/^#//g' /etc/pacman.conf
-sudo pacman -Syu
 sudo pacman -S lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader lib32-gnutls
