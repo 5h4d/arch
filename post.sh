f46ee42ebc680b4bf10bfcfaf1c499014c040bab
5h4d <62404657+5h4d@users.noreply.github.com>
2020-11-23 18:31:13 +0100

    Update post.sh

--- post.sh
+++ post.sh
@@ -4,10 +4,11 @@ sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
 sed -i '/sk_SK.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
-echo 'arch' >> /etc/hostname
+read -p 'What do you want this system to be called?: ' archname
+echo $archname >> /etc/hostname
 echo '127.0.0.1     localhost' >> /etc/hosts
 echo '::1           localhost' >> /etc/hosts
-echo '127.0.1.1     arch.localdomain    arch' >> /etc/hosts
+echo '127.0.1.1     '$archname'.localdomain    '$archname >> /etc/hosts
 echo 'Root password'
 passwd
 read -p 'What name do you want the regular user to have?: ' username
@@ -19,6 +20,7 @@ sed -i '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers
 chown $username:$username /home/user
 chown $username:$username /home/user/gui-programs.sh
 chown $username:$username /home/user/extensions.zip
+chown $username:$username /home/user/discordsoundshare.sh
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
