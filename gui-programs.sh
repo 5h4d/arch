ac67a014b871f60d2089425a703847de98622d3f
5h4d <62404657+5h4d@users.noreply.github.com>
2020-11-23 20:23:24 +0100

    Update gui-programs.sh

--- gui-programs.sh
+++ gui-programs.sh
@@ -11,7 +11,11 @@ git clone https://aur.archlinux.org/gnome-shell-extension-ubuntu-dock.git
 git clone https://aur.archlinux.org/idle-python3.7-assets.git
 git clone https://aur.archlinux.org/spotify.git
 git clone https://aur.archlinux.org/gdm-plymouth.git
+git clone https://aur.archlinux.org/yay-git.git
 cd gnome-shell-extension-ubuntu-dock
+makepkg -si
+cd ..
+cd yay-git
 makepkg -si
 cd ..
 cd idle-python3.7-assets
@@ -24,7 +28,7 @@ cd ..
 cd gdm-plymouth
 makepkg -si
 cd ..
-sudo rm -r spotify gnome-shell-extension-ubuntu-dock idle-python3.7-assets gdm-plymouth
+sudo rm -r spotify gnome-shell-extension-ubuntu-dock idle-python3.7-assets gdm-plymouth yay-git
 unzip -d ~/.local/share/gnome-shell/extensions extensions.zip
 rm extensions.zip
 sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs
