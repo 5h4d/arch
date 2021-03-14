337da4f542a369910d8ad3fdee86ff855e2ff61f
5h4d <62404657+5h4d@users.noreply.github.com>
2021-03-14 09:12:52 +0100

    Update gui-programs.sh

--- gui-programs.sh
+++ gui-programs.sh
@@ -6,7 +6,7 @@ read tmpvar
 sudo sed -i '/^#\[multilib]/{n;s/^#//}' /etc/pacman.conf
 sudo sed -i 's/#\[multilib]/\[multilib]/g' /etc/pacman.conf
 sudo pacman -Syu
-sudo pacman -S lutris steam discord gparted qbittorrent jdk-openjdk gimp pulseaudio pavucontrol python python-pip fish vlc
+sudo pacman -S lutris steam discord gparted qbittorrent jdk-openjdk gimp pulseaudio pavucontrol python python-pip fish vlc gtk-engine-murrine gtk-engines
 pip install discover-overlay
 git clone https://aur.archlinux.org/gnome-shell-extension-ubuntu-dock.git
 git clone https://aur.archlinux.org/idle-python3.7-assets.git
@@ -15,6 +15,7 @@ git clone https://aur.archlinux.org/gdm-plymouth.git
 git clone https://aur.archlinux.org/yay-git.git
 git clone https://aur.archlinux.org/ffmpeg-compat-57.git
 git clone https://aur.archlinux.org/brave-bin.git
+git clone https://github.com/vinceliuice/Layan-gtk-theme
 cd gnome-shell-extension-ubuntu-dock
 makepkg -si
 cd ..
@@ -37,7 +38,10 @@ cd ..
 cd brave-bin
 makepkg -si
 cd ..
-sudo rm -r spotify gnome-shell-extension-ubuntu-dock idle-python3.7-assets gdm-plymouth yay-git ffmpeg-compat-57 brave-bin
+cd Layan-gtk-theme
+./install.sh
+cd ..
+sudo rm -r spotify gnome-shell-extension-ubuntu-dock idle-python3.7-assets gdm-plymouth yay-git ffmpeg-compat-57 brave-bin Layan-gtk-theme
 unzip -d ~/.local/share/gnome-shell/ extensions.zip
 rm extensions.zip
 sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs
