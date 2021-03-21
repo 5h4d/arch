8e5010ec8c96279585c9319f5738a4fe9788b25e
5h4d <62404657+5h4d@users.noreply.github.com>
2021-03-21 09:51:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ echo The system is going to install arch on $disk and dedicate $swapsize of stor
 read tmpvar
 
 pacman -S wipe
-y
 wipe $disk
 timedatectl set-ntp true
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $disk
