acef054d6bf604e08101068af90c98c3600dd570
5h4d <62404657+5h4d@users.noreply.github.com>
2021-03-21 11:25:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 read -p 'What drive would you like to install to? (example: /dev/sda) Make sure the format is correct. : ' disk
 
-read -p 'What amount of storage would you like to dedicate to swap?(example: 20G) make sure the format is correct: ' swapsize
+read -p 'How much space on the disk would you like to dedicate to swap?(example: 20G) make sure the format is correct: ' swapsize
 echo The system is going to install arch on $disk and dedicate $swapsize of storage to swap. Ctrl+C to cancel or press enter to continue.
 read tmpvar
 
@@ -16,33 +16,36 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $disk
   n
   
   
+  +$swapsize
+  n
+  
+  
   
   t
   1
   1
+  t
+  2
+  19
   w
 EOF
-mkfs.btrfs $disk'2'
+mkfs.btrfs $disk'3'
 mkfs.fat -F32 $disk'1'
-mount $disk'2' /mnt
+mount $disk'3' /mnt
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
-btrfs su cr /mnt/@swap
 btrfs su cr /mnt/@snapshots
 umount -l /mnt
-mount -o noatime,compress=lzo,subvol=@ $disk'2' /mnt/
-mkdir -p /mnt/{boot,home,swap,.snapshots}
-mount -o noatime,compress=lzo,subvol=@home $disk'2' /mnt/home
-mount -o noatime,compress=lzo,subvol=@swap $disk'2' /mnt/swap
-mount -o noatime,compress=lzo,subvol=@snapshots $disk'2' /mnt/.snapshots
+mount -o noatime,compress=lzo,subvol=@ $disk'3' /mnt/
+mkdir -p /mnt/{boot,home,.snapshots}
+mount -o noatime,compress=lzo,subvol=@home $disk'3' /mnt/home
+mount -o noatime,compress=lzo,subvol=@snapshots $disk'3' /mnt/.snapshots
 mount $disk'1' /mnt/boot/
-fallocate -l $swapsize /mnt/swap/swapfile
-chmod 600 /mnt/swap/swapfile
-mkswap /mnt/swap/swapfile
-swapon /mnt/swap/swapfile
+mkswap $disk'2'
+swapon $disk'2'
 pacstrap /mnt base base-devel linux linux-firmware git vim gnome networkmanager grub efibootmgr dosfstools mtools neofetch cups btrfs-progs grub-btrfs snapper
 genfstab -U /mnt >> /mnt/etc/fstab
-mv post.sh hom root /mnt
+mv post.sh hom rot /mnt
 chmod +x /mnt/post.sh
 echo 'After you are chrooted execute the second part of the script with ./post.sh'
 echo 'Press enter to chroot...'
