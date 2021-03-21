ee939ea12b0b2785394fa468a335e6eaf5d7eefb
5h4d <62404657+5h4d@users.noreply.github.com>
2021-03-21 12:25:31 +0100

    Update snapper-config.sh

--- snapper-config.sh
+++ snapper-config.sh
@@ -4,11 +4,11 @@ sudo rm -rf /.snapshots
 sudo snapper -c root create-config /
 sudo snapper -c home create-config /home
 sudo chmod a+rx /.snapshots
-sudo chown :$username /.snapshots
+sudo chown :$USER /.snapshots
 sudo rm /etc/snapper/configs/root
 sudo rm /etc/snapper/configs/home
 sudo mv rot /etc/snapper/configs/root
 sudo mv hom /etc/snapper/configs/home
 sudo ffff=$(echo "ffff" | sed -e 's/[]$.*[\^]/\\&/g' )
-sudo sed -i -e "s/ffff/${user}/g" /etc/snapper/configs/root
-sudo sed -i -e "s/ffff/${user}/g" /etc/snapper/configs/home
+sudo sed -i -e "s/ffff/${USER}/g" /etc/snapper/configs/root
+sudo sed -i -e "s/ffff/${USER}/g" /etc/snapper/configs/home
