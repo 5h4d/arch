3719bc4e2e9abf21db12066d478e7de4d9a6ebbb
5h4d <62404657+5h4d@users.noreply.github.com>
2021-03-21 11:47:20 +0100

    Create snapper-config.sh

---
+++ snapper-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo su
+umount -l /.snapshots
+rm -rf /.snapshots
+snapper -c root create-config /
+snapper -c home create-config /home
+chmod a+rx /.snapshots
+chown :$username /.snapshots
+rm /etc/snapper/configs/root
+rm /etc/snapper/configs/home
+mv rot /etc/snapper/configs/root
+mv hom /etc/snapper/configs/home
+ffff=$(echo "ffff" | sed -e 's/[]$.*[\^]/\\&/g' )
+sed -i -e "s/ffff/${user}/g" /etc/snapper/configs/root
+sed -i -e "s/ffff/${user}/g" /etc/snapper/configs/home
+exit
