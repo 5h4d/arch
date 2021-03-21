6f4e6986bd2217f190e592b4b0ce76c6f1a73706
5h4d <62404657+5h4d@users.noreply.github.com>
2021-03-21 11:38:34 +0100

    Update post.sh

--- post.sh
+++ post.sh
@@ -29,17 +29,4 @@ systemctl enable cups.service
 systemctl enable snapper-timeline.timer
 systemctl enable snapper-cleanup-timer
 systemctl enable grub-btrfs.path
-umount -l /.snapshots
-rm -rf /.snapshots
-snapper -c root create-config /
-snapper -c home create-config /home
-chmod a+rx /.snapshots
-chown :$username /.snapshots
-rm /etc/snapper/configs/root
-rm /etc/snapper/configs/home
-mv rot /etc/snapper/configs/root
-mv hom /etc/snapper/configs/home
-ffff=$(echo "ffff" | sed -e 's/[]$.*[\^]/\\&/g' )
-sed -i -e "s/ffff/${username}/g" /etc/snapper/configs/root
-sed -i -e "s/ffff/${username}/g" /etc/snapper/configs/home
 echo 'All done, exit chroot and off you go'
