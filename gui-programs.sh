6f08be6aef50fd4e0e01861e643156cf617bec4d
5h4d <62404657+5h4d@users.noreply.github.com>
2021-03-30 12:38:28 +0200

    Update gui-programs.sh

--- gui-programs.sh
+++ gui-programs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Execute this script once you boot into the gui.
-echo 'This script will install: wine, lutris, steam, discord, gparted, qbittorrent, spotify, brave, python, idle, gimp, java, vlc and some gnome extensions'
+echo 'This script will install: wine, lutris, steam, discord, gparted, qbittorrent, brave, python, idle, gimp, java, vlc and some gnome extensions'
 echo 'Tap enter to continue...'
 read tmpvar
 sudo sed -i '/^#\[multilib]/{n;s/^#//}' /etc/pacman.conf
@@ -10,10 +10,8 @@ sudo pacman -S lutris steam discord gparted qbittorrent jdk-openjdk gimp pulseau
 pip install discover-overlay
 git clone https://aur.archlinux.org/gnome-shell-extension-ubuntu-dock.git
 git clone https://aur.archlinux.org/idle-python3.7-assets.git
-git clone https://aur.archlinux.org/spotify.git
 git clone https://aur.archlinux.org/gdm-plymouth.git
 git clone https://aur.archlinux.org/yay-git.git
-git clone https://aur.archlinux.org/ffmpeg-compat-57.git
 git clone https://aur.archlinux.org/brave-bin.git
 git clone https://github.com/vinceliuice/Layan-gtk-theme
 git clone http://aur.archlinux.org/snapper-gui-git.git
@@ -24,13 +22,6 @@ cd yay-git
 makepkg -si
 cd ..
 cd idle-python3.7-assets
-makepkg -si
-cd ..
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
-cd ffmpeg-compat-57
-makepkg -si
-cd ..
-cd spotify
 makepkg -si
 cd ..
 cd gdm-plymouth
@@ -45,7 +36,7 @@ cd ..
 cd snapper-gui-git
 makepkg -si
 cd ..
-sudo rm -r spotify gnome-shell-extension-ubuntu-dock idle-python3.7-assets gdm-plymouth yay-git ffmpeg-compat-57 brave-bin Layan-gtk-theme snapper-gui-git
+sudo rm -r gnome-shell-extension-ubuntu-dock idle-python3.7-assets gdm-plymouth yay-git brave-bin Layan-gtk-theme snapper-gui-git
 unzip -d ~/.local/share/gnome-shell/ extensions.zip
 rm extensions.zip
 sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs
